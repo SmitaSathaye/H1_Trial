648360210ff557fe7fd7442bbccad42d5ad1b6ae
Smita <smi.sathaye@gmail.com>
2022-12-26 17:05:45 +0100

    Pipeline 24

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
@@ -35,7 +36,10 @@ module.exports = defineConfig({
       inlineAssets: true,
       saveAllAttempts: false,
       reportFilename: 'Saucedemo Report',
-    },        
+    },
+    env: {
+      allureReuseAfterSpec: true,
+    },    
   }, 
   
 });
